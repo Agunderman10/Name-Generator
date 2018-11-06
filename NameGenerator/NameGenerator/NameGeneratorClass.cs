aa3ea9bd4dcbe7a24bb834b5644a4288d8566176
Andrew Gunderman <agunderman10@gmail.com>
2018-11-06 08:14:07 -0500

    -Initial code commit -Adds all functionality for working program -Adds static Array name containers -Inherits from Random class for indexing arrays

---
+++ NameGenerator/NameGenerator/NameGeneratorClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace NameGenerator
+{
+    class NameGeneratorClass : NamesContainer
+    {
+        static void Main(string[] args)
+        {
+
+            //asks user for desired name gender
+            Console.WriteLine("Are you looking for a boy or girl name?");
+            //assigns user specified gender to gender variable
+            string gender = Console.ReadLine();
+
+            //initilizes random class
+            Random rand = new Random();
+
+            
+            //if user wants a boy name...
+            if(gender.Equals("boy") || gender.Equals("Boy"))
+            {
+                //sets index of the array to a random number in between 0 and BoysNames length
+                int BoysIndex = rand.Next(0, BoysNames.Length);
+                //prints a random index of the array as a name
+                Console.WriteLine(BoysNames[BoysIndex]);
+            }
+            //if user wants a girl name...
+            else if(gender.Equals("girl") || gender.Equals("Girl"))
+            {
+                //sets index of the array to a random number in between 0 and GirlsNames length
+                int GirlsIndex = rand.Next(0, GirlsNames.Length);
+                //prints a random index of the array as a name
+                Console.WriteLine(GirlsNames[GirlsIndex]);
+            }
+            else
+            {
+                Console.WriteLine("Error. Gender is invalid. Make sure you specified either a 'boy' or a 'girl'.");
+            }
+
+            //keeps the console window from closing
+            while(true)
+            {
+                string exitCommand = Console.ReadLine();
+                if(exitCommand.Equals("exit") || exitCommand.Equals("Exit"))
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
---
+++ NameGenerator/NameGenerator/NamesContainer.cs
@@ -0,0 +1,75 @@
+﻿namespace NameGenerator
+{
+    class NamesContainer
+    {
+        //initializes array of boy names
+        public readonly static string[] BoysNames = {
+            "Andrew",
+            "Jim",
+            "John",
+            "Able",
+            "Aaron",
+            "Devin",
+            "Devon",
+            "Joe",
+            "Michael",
+            "Sam",
+            "Henry",
+            "Jocob",
+            "Edward",
+            "Grant",
+            "Caleb",
+            "Augustus",
+            "Auggie",
+            "Hunter",
+            "Andy",
+            "Sander",
+            "Zander",
+            "Rory",
+            "Harry",
+            "Ron",
+            "Ethan",
+            "Tyler",
+            "Titus",
+            "Mike",
+            "Jesse",
+            "Garrett",
+            "Antonio"
+        };
+
+        //initializes array of girl names
+        public readonly static string[] GirlsNames = {
+            "Jessica",
+            "Abby",
+            "Abigail", 
+            "Desiree",
+            "Lana",
+            "Callan",
+            "Samantha",
+            "Sarah",
+            "Abi",
+            "Aleisha",
+            "Keisha",
+            "Amy",
+            "Lauren",
+            "Lori",
+            "Sandy",
+            "Sal",
+            "Hermione",
+            "Kaylee",
+            "Kayla",
+            "Serene",
+            "Fiona",
+            "Taylor",
+            "Emily",
+            "Emryn",
+            "Sassy",
+            "Amanda",
+            "Jordan",
+            "Johanna",
+            "Kenzie",
+            "Kristin",
+            "Kirstin"
+        };
+    }
+}
